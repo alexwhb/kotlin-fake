51ba30ce943b0ba648d8e383dfb6719dec364333
Alexander Black <alexwhb@gmail.com>
2020-09-16 16:35:48 -0700

    Adds ColorProvider.kt

--- src/commonMain/kotlin/providers/definition/ColorProvider.kt
+++ src/commonMain/kotlin/providers/definition/ColorProvider.kt
@@ -1,5 +1,14 @@
 package providers.definition
 
-interface ColorProvider {
-
+interface ColorProvider : Provider{
+    val hexColor: String
+    val safeHexColor: String
+    val rgbColor: String
+    val rgbColorArray: List<Int>
+    val cssRgbColor: String
+    val rgbaCssColor: String
+    val safeColorName: String
+    val colorName: String
+    val hslColor: String
+    val hslColorArray: List<Int>
 }
