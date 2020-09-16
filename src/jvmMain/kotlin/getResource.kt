fde709f6bfb8b76fa5a63f370547417db95c7c84
Alexander Black <alexwhb@gmail.com>
2020-09-16 16:33:13 -0700

    Updates getResource.kt with string format multiplatform implementation.

--- src/jvmMain/kotlin/getResource.kt
+++ src/jvmMain/kotlin/getResource.kt
@@ -1,6 +1,13 @@
+import java.util.*
 import kotlin.reflect.KClass
 
 
 actual fun <T : Any> getResourcePath(klass: KClass<T>, path: String):String {
    return  klass.java.getResource(path).path ?: ""
+}
+
+actual fun String.format(local: String, vararg args: Any?):String {
+   // yes I know I'm not using the local... for now I'm just defaulting for simplicity sake.
+   // if I remove that param it will confuse the correct method resolver.
+   return this.format(*args)
 }
--- src/nativeMain/kotlin/getResource.kt
+++ src/nativeMain/kotlin/getResource.kt
@@ -1,5 +1,11 @@
 import kotlin.reflect.KClass
 
+
 actual fun <T : Any> getResourcePath(klass: KClass<T>, path: String): String {
    return "" // TODO
+}
+
+
+actual fun String.format(local: String, vararg args: Any?): String {
+    return "" // todo this needs to be implemented. 
 }
