6e16b4431dea0e0765f7afcadc08f23dc34c0fb8
Alexander Black <alexwhb@gmail.com>
2020-09-15 18:17:01 -0700

    Adds MiscellaneousProviderImp.kt

---
+++ src/commonMain/kotlin/providers/MiscellaneousProviderImp.kt
@@ -0,0 +1,29 @@
+package providers
+
+import providers.definition.MiscellaneousProvider
+
+class MiscellaneousProviderImp : MiscellaneousProvider {
+    override val boolean: Boolean
+        get() = TODO("Not yet implemented")
+
+    override fun boolean(chanceOfGettingTrue: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override val md5: String
+        get() = TODO("Not yet implemented")
+    override val sha1: String
+        get() = TODO("Not yet implemented")
+    override val sha256: String
+        get() = TODO("Not yet implemented")
+    override val locale: String
+        get() = TODO("Not yet implemented")
+    override val countryCode: String
+        get() = TODO("Not yet implemented")
+    override val languageCode: String
+        get() = TODO("Not yet implemented")
+    override val currencyCode: String
+        get() = TODO("Not yet implemented")
+    override val emoji: String
+        get() = TODO("Not yet implemented")
+}
