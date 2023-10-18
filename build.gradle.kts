626d8bf976cf046ceefe21c9708fcdcd2d1d1c9c
Alex Black <alexwhb@gmail.com>
2023-10-17 18:19:52 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
-    kotlin("multiplatform") version "1.5.30"
-    kotlin("plugin.serialization") version "1.5.30"
-//    kotlin("native.cocoapods") version "1.5.21"
+    kotlin("multiplatform") version "1.8.0"
+    kotlin("plugin.serialization") version "1.5.0"
     id("maven-publish")
-//    id("org.jetbrains.dokka") version "1.5.30"
 }
 group = "com.blackstone"
 version = "0.1.5"
@@ -15,28 +13,17 @@ repositories {
     mavenCentral()
 }
 kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-    }
-
-    val iosTarget: (String, org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget.() -> Unit) -> org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget =
-        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
-            ::iosArm64
-        else
-            ::iosX64
-
-    iosTarget("ios") {}
-
+    jvm()
+    ios()
+    
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("net.mamoe.yamlkt:yamlkt:0.10.0")
+                implementation("net.mamoe.yamlkt:yamlkt:0.13.0")
                 implementation("com.soywiz.korlibs.krypto:krypto:2.2.0")
-                implementation( "com.benasher44:uuid:0.2.2")
+                implementation( "com.benasher44:uuid:0.6.0")
                 implementation("com.soywiz.korlibs.klock:klock:2.2.0")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.0")
             }
 
         }
